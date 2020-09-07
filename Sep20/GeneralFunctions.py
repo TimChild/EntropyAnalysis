30e018fbd8d8b73be8978ece314b194613ee9d16
Tim Child <timjchild@phas.ubc.ca>
2020-09-07 14:29:09 -0700

    fixed the merge_dat_parts thing

--- Sep20/GeneralFunctions.py
+++ Sep20/GeneralFunctions.py
@@ -25,14 +25,16 @@ def merge_dat_parts(dats, centers=None):  # TODO: This could be a lot more gener
         elif part_num == 2:
             p2_dat = dat
 
-    full_x = p1_dat.SquareEntropy.Processed.outputs.x
+    full_x = p1_dat.Data.x_array
     p1_data = p1_dat.SquareEntropy.Processed.outputs.cycled
-
-    p2_x = p2_dat.SquareEntropy.Processed.outputs.x
+    full_x = CU.get_matching_x(full_x, p1_data)
+    
+    p2_x = p2_dat.Data.x_array
     p2_data = p2_dat.SquareEntropy.Processed.outputs.cycled
-    idxs = CU.get_data_index(full_x, [p2_x[0], p2_x[-1]])
-
-    p2_data = np.pad(p2_data, ((0, 0), (0, 0), (idxs[0], full_x.shape[0]-idxs[1]-1)), mode='constant', constant_values=np.nan)
+    p2_x = CU.get_matching_x(p2_x, p2_data)
+    idx = CU.get_data_index(full_x, p2_x[0])
+          
+    p2_data = np.pad(p2_data, ((0, 0), (0, 0), (idx, full_x.shape[0]-p2_x.shape[0]-idx)), mode='constant', constant_values=np.nan)
 
     full_data = np.concatenate((p1_data, p2_data), axis=0)
     full_x, full_avg_data = SE.average_2D(full_x, full_data, avg_nans=True, centers=centers)
